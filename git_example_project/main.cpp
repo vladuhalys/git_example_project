99730cdfd32599240519232c9f393ac61233e9c2
vladuhalys <vlad.lysenko1999@gmail.com>
2024-03-09 20:08:57 +0200

    Lab ex 1 - Done

--- git_example_project/main.cpp
+++ git_example_project/main.cpp
@@ -6,6 +6,12 @@ using std::cin;
 
 int main()
 {
-	
+	cout << "Hello, World!" << endl;
+	cout << "Press Enter to continue...";
+	for (size_t i = 0; i < 10; i++)
+	{
+		cout << &i << endl;
+	}
+	cin.get();
 	return 0;
 }
